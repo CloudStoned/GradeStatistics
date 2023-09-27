d19e4181ed40d403c509a279b1af7e6d1367fca9
CloudStone <cloudneo0719@gmail.com>
2023-09-27 14:34:24 +0800

    Initial Commit

---
+++ GetMean.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvStatics
+{
+    public class GetMean
+    {
+        private static double CalculatMean(List<StudentModel> studentList)
+        {
+            double sum = 0;
+            int counter = 0;
+            double mean;
+
+            foreach (var student in studentList)
+            {
+                sum += student.prelim;
+                counter++;
+            }
+
+            mean = sum / counter;
+
+            return mean;
+        }
+
+        public static void DisplayPrelimMean(List<StudentModel> studentList)
+        {
+            Console.Write("Prelim Mean");
+            double prelimMean = CalculatMean(studentList);
+            Console.Write($"Prelim Mean: {prelimMean:2F}");
+        }
+
+        public static void DisplayMidtermMean(List<StudentModel> studentList)
+        {
+            double midtermMean = CalculatMean(studentList);
+            Console.Write($"Midterm Mean: {midtermMean:2F}");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvStatics
+{
+    public class StudentModel
+    {
+        public string studentName { get; set; }
+        public double prelim { get; set;}
+        public double midterm { get; set; }
+        public double finals { get; set; }
+    }
+
+
+
+
+
+}
