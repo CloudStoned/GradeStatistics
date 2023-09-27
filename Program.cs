96c9322d0a70de46efacbd0437ffe893f3624d4a
CloudStone <cloudneo0719@gmail.com>
2023-09-27 14:43:46 +0800

    Setter and Getter Testing

--- Program.cs
+++ Program.cs
@@ -12,50 +12,24 @@ namespace AdvStatics
         {
             List<StudentModel> studentsList = new List<StudentModel>();
             {
+                new StudentModel()
+                {
+                    studentName = "Student A",
+                    prelim = 78.88,
+                    midterm = 85.00,
+                    finals = 100.00
+                };
 
-            StudentModel studentA = new StudentModel();
-            studentA.studentName = "Student A";
-            studentA.prelim = 78.88;
-            studentA.midterm = 85.00;
-            studentA.finals = 100.00;
-            studentsList.Add(studentA);
+                new StudentModel()
+                {
+                    studentName = "Student B",
+                    prelim = 56.76,
+                    midterm = 98.00,
+                    finals = 100.00
+                };
 
-            StudentModel studentB = new StudentModel();
-            studentB.studentName = "Student B";
-            studentB.prelim = 56.76;
-            studentB.midterm = 98.00;
-            studentB.finals = 100.00;
-            studentsList.Add(studentB);
-
-            
-            StudentModel studentC = new StudentModel();
-            studentC.studentName = "Student C";
-            studentC.prelim = 98.00;
-            studentC.midterm = 87.92;
-            studentC.finals = 99.00;
-            studentsList.Add(studentC);
-
-            StudentModel studentD = new StudentModel();
-            studentD.studentName = "Student D";
-            studentD.prelim = 87.98;
-            studentD.midterm = 85.00;
-            studentD.finals = 98.00;
-            studentsList.Add(studentD);
-
-            StudentModel studentE = new StudentModel();
-            studentE.studentName = "Student E";
-            studentE.prelim = 89.00;
-            studentE.midterm = 90.15;
-            studentE.finals = 97.00;
-            studentsList.Add(studentE);
-
-            StudentModel studentF = new StudentModel();
-            studentF.studentName = "Student F";
-            studentF.prelim = 90.00;
-            studentF.midterm = 90.11;
-            studentF.finals = 89.90;
-            studentsList.Add(studentF);
-
+             
+            }
 
 
 
@@ -68,5 +42,5 @@ namespace AdvStatics
             //}
 
         }
-    }
+   }
 }
