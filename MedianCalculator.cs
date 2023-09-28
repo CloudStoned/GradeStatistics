d3880afd80d3104f2850b6dc8010b3d623e987ea
CloudStone <cloudneo0719@gmail.com>
2023-09-28 18:42:45 +0800

    MedianCalculator Added In Progress

---
+++ MedianCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvStatics
+{
+    public class MedianCalculator
+    {
+        private static (List<StudentModel>, int countData) SortStudentList(List<StudentModel> students)
+        {
+            int count = 0;
+            var sortedStudentList = students.OrderBy(student => student.prelim).ToList();
+            count = sortedStudentList.Count;
+
+            return (sortedStudentList, count);
+        }
+
+        private static List<StudentModel> CalculateMedian(List<StudentModel> studentList, Func<StudentModel, double> termSelector)
+        {
+            var sortedList = SortStudentList(studentList).Item1; // Extract the sorted list from the tuple
+
+            // Implement median calculation logic here if needed
+
+            return sortedList;
+        }
+
+        public static void DisplayPrelimMedian(List<StudentModel> students)
+        {
+            var sortedList = CalculateMedian(students, student => student.prelim);
+
+            Console.WriteLine("Sorted List:");
+            foreach (var student in sortedList)
+            {
+                Console.WriteLine(student.prelim);
+            }
+
+            Console.WriteLine($"{students.Count} Data");
+        }
+    }
+}
