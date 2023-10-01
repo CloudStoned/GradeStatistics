de90a0e9cc2a0a70bcb27cb3c89c665fc6fffd4c
CloudStone <cloudneo0719@gmail.com>
2023-10-01 23:08:26 +0800

    Added ModeCalculator

---
+++ ModeCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvStatics
+{
+    public class ModeCalculator
+    {
+        private static double HasMode(List<StudentModel> studentList,Func<StudentModel, double> termSelector)
+        {
+            double currentValue = termSelector(studentList[0]);
+            int consecutiveCount = 1;
+
+            for (int i = 1; i < studentList.Count; i++)
+            {
+                double nextValue = termSelector(studentList[i]);
+
+                if (currentValue == nextValue)
+                {
+                    consecutiveCount++;
+                }
+                else
+                {
+                    consecutiveCount = 1;
+                }
+
+                if (consecutiveCount > 1)
+                {
+                    return currentValue;
+                }
+
+                currentValue = nextValue;
+            }
+
+            return 0;
+        }
+
+        public static void DisplayPrelimMode(List<StudentModel> studentList) 
+        {
+            double prelimMode = HasMode(studentList, student => student.prelim);
+            Console.WriteLine($"Prelim Mode : {prelimMode}");
+        }
+
+        public static void DisplayMidtermMode(List<StudentModel> studentList)
+        {
+            double midtermMode = HasMode(studentList, student => student.midterm);
+            Console.WriteLine($"Midterm Mode: {midtermMode}");
+        }
+
+        public static void DisplayFinalsMode(List<StudentModel> studentList)
+        {
+            double finalsMode = HasMode(studentList, student => student.finals);
+            Console.WriteLine($"Finals Mode : {finalsMode}");
+        }
+
+    }
+}
