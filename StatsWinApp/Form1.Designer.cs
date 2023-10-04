62c68e737fdc5b483b5cbda550f215745ed6e6a1
CloudStone <cloudneo0719@gmail.com>
2023-10-04 14:19:05 +0800

    Added Mean Button

--- StatsWinApp/Form1.Designer.cs
+++ StatsWinApp/Form1.Designer.cs
@@ -29,33 +29,36 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            button1 = new Button();
+            CalculateMean_BTN = new Button();
             listView1 = new ListView();
             StudentName = new ColumnHeader();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(351, 157);
+            label1.Location = new Point(345, 49);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
             label1.Text = "label1";
             // 
-            // button1
+            // CalculateMean_BTN
             // 
-            button1.Location = new Point(149, 66);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            CalculateMean_BTN.Location = new Point(345, 12);
+            CalculateMean_BTN.Name = "CalculateMean_BTN";
+            CalculateMean_BTN.Size = new Size(89, 23);
+            CalculateMean_BTN.TabIndex = 1;
+            CalculateMean_BTN.Text = "Calculate Mean";
+            CalculateMean_BTN.UseVisualStyleBackColor = true;
+            CalculateMean_BTN.Click += button1_Click;
             // 
             // listView1
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { StudentName });
-            listView1.Location = new Point(115, 184);
+            listView1.Location = new Point(12, 12);
             listView1.Name = "listView1";
             listView1.Size = new Size(291, 171);
             listView1.TabIndex = 2;
@@ -67,13 +70,33 @@
             StudentName.Text = "Student Name";
             StudentName.Width = 110;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(345, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(345, 106);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(listView1);
-            Controls.Add(button1);
+            Controls.Add(CalculateMean_BTN);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -85,8 +108,10 @@
         #endregion
 
         private Label label1;
-        private Button button1;
+        private Button CalculateMean_BTN;
         private ListView listView1;
         private ColumnHeader StudentName;
+        private Label label2;
+        private Label label3;
     }
 }
--- StatsWinApp/Form1.cs
+++ StatsWinApp/Form1.cs
@@ -1,4 +1,5 @@
 using StatsDLL;
+using static System.Windows.Forms.AxHost;
 
 namespace StatsWinApp
 {
@@ -26,6 +27,25 @@ namespace StatsWinApp
                 listView1.Items.Add(listViewItems);
 
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double pMean = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.prelim);
+            double mMean = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.midterm);
+            double fMean = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.finals);
+
+
+            label1.Text = $"Prelim Mean: {pMean:F3}";
+            label1.Visible = true;
+
+
+            label2.Text = $"Midterm Mean: {mMean:F3}";
+            label2.Visible = true;
+
+
+            label3.Text = $"Finals Mean: {fMean:F3}";
+            label3.Visible = true;
         }
     }
 }
