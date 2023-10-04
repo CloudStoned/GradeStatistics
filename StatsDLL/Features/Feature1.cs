7dddf714b97d2158e4cb5d9a122877f82e8447ec
CloudStone <cloudneo0719@gmail.com>
2023-10-04 13:59:44 +0800

    Added Test Feature

---
+++ StatsDLL/Features/Feature1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatsDLL.Features
+{
+    public class Feature1
+    {
+        public static void DisplayName()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
