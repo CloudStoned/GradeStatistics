38d0ebf644493879b1aba5b5839620abe4339ed3
CloudStone <cloudneo0719@gmail.com>
2023-10-04 14:30:49 +0800

    Added Median & Mode Buttons

--- StatsWinApp/Form1.Designer.cs
+++ StatsWinApp/Form1.Designer.cs
@@ -32,14 +32,25 @@
             CalculateMean_BTN = new Button();
             listView1 = new ListView();
             StudentName = new ColumnHeader();
+            Prelim = new ColumnHeader();
+            Midterm = new ColumnHeader();
+            Finals = new ColumnHeader();
             label2 = new Label();
             label3 = new Label();
+            CalculateMedian_BTN = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            CalculateMode_BTN = new Button();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(345, 49);
+            label1.Location = new Point(332, 49);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
@@ -47,9 +58,9 @@
             // 
             // CalculateMean_BTN
             // 
-            CalculateMean_BTN.Location = new Point(345, 12);
+            CalculateMean_BTN.Location = new Point(332, 12);
             CalculateMean_BTN.Name = "CalculateMean_BTN";
-            CalculateMean_BTN.Size = new Size(89, 23);
+            CalculateMean_BTN.Size = new Size(123, 23);
             CalculateMean_BTN.TabIndex = 1;
             CalculateMean_BTN.Text = "Calculate Mean";
             CalculateMean_BTN.UseVisualStyleBackColor = true;
@@ -57,10 +68,10 @@
             // 
             // listView1
             // 
-            listView1.Columns.AddRange(new ColumnHeader[] { StudentName });
+            listView1.Columns.AddRange(new ColumnHeader[] { StudentName, Prelim, Midterm, Finals });
             listView1.Location = new Point(12, 12);
             listView1.Name = "listView1";
-            listView1.Size = new Size(291, 171);
+            listView1.Size = new Size(295, 249);
             listView1.TabIndex = 2;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
@@ -70,10 +81,22 @@
             StudentName.Text = "Student Name";
             StudentName.Width = 110;
             // 
+            // Prelim
+            // 
+            Prelim.Text = "Prelim";
+            // 
+            // Midterm
+            // 
+            Midterm.Text = "Midterm";
+            // 
+            // Finals
+            // 
+            Finals.Text = "Finals";
+            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(345, 77);
+            label2.Location = new Point(332, 77);
             label2.Name = "label2";
             label2.Size = new Size(38, 15);
             label2.TabIndex = 3;
@@ -82,17 +105,99 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(345, 106);
+            label3.Location = new Point(332, 106);
             label3.Name = "label3";
             label3.Size = new Size(38, 15);
             label3.TabIndex = 4;
             label3.Text = "label3";
             // 
+            // CalculateMedian_BTN
+            // 
+            CalculateMedian_BTN.Location = new Point(461, 12);
+            CalculateMedian_BTN.Name = "CalculateMedian_BTN";
+            CalculateMedian_BTN.Size = new Size(123, 23);
+            CalculateMedian_BTN.TabIndex = 5;
+            CalculateMedian_BTN.Text = "Calculate Median";
+            CalculateMedian_BTN.UseVisualStyleBackColor = true;
+            CalculateMedian_BTN.Click += CalculateMedian_BTN_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(471, 106);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 8;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(471, 77);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 7;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(471, 49);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 6;
+            label6.Text = "label6";
+            // 
+            // CalculateMode_BTN
+            // 
+            CalculateMode_BTN.Location = new Point(590, 12);
+            CalculateMode_BTN.Name = "CalculateMode_BTN";
+            CalculateMode_BTN.Size = new Size(123, 23);
+            CalculateMode_BTN.TabIndex = 9;
+            CalculateMode_BTN.Text = "Calculate Mode";
+            CalculateMode_BTN.UseVisualStyleBackColor = true;
+            CalculateMode_BTN.Click += CalculateMode_BTN_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(590, 106);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 12;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(590, 77);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 11;
+            label8.Text = "label8";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(590, 49);
+            label9.Name = "label9";
+            label9.Size = new Size(38, 15);
+            label9.TabIndex = 10;
+            label9.Text = "label9";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1331, 713);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(CalculateMode_BTN);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(CalculateMedian_BTN);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(listView1);
@@ -113,5 +218,16 @@
         private ColumnHeader StudentName;
         private Label label2;
         private Label label3;
+        private ColumnHeader Prelim;
+        private ColumnHeader Midterm;
+        private ColumnHeader Finals;
+        private Button CalculateMedian_BTN;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button CalculateMode_BTN;
+        private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
--- StatsWinApp/Form1.cs
+++ StatsWinApp/Form1.cs
@@ -21,8 +21,7 @@ namespace StatsWinApp
         {
             foreach (StudentModel student in StudentModel.studentsList)
             {
-                Console.WriteLine(student.studentName);
-                string[] rows = { student.studentName };
+                string[] rows = { student.studentName, student.prelim.ToString(), student.midterm.ToString(), student.finals.ToString() };
                 var listViewItems = new ListViewItem(rows);
                 listView1.Items.Add(listViewItems);
 
@@ -31,21 +30,59 @@ namespace StatsWinApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double pMean = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.prelim);
-            double mMean = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.midterm);
-            double fMean = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.finals);
+            double prelim = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.prelim);
+            double midterm = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.midterm);
+            double finals = MeanCalculator.CalculateMean(StudentModel.studentsList, stud => stud.finals);
 
 
-            label1.Text = $"Prelim Mean: {pMean:F3}";
+            label1.Text = $"Prelim Mean: {prelim:F3}";
             label1.Visible = true;
 
 
-            label2.Text = $"Midterm Mean: {mMean:F3}";
+            label2.Text = $"Midterm Mean: {midterm:F3}";
             label2.Visible = true;
 
 
-            label3.Text = $"Finals Mean: {fMean:F3}";
+            label3.Text = $"Finals Mean: {finals:F3}";
             label3.Visible = true;
+        }
+
+        private void CalculateMedian_BTN_Click(object sender, EventArgs e)
+        {
+            double prelim = MedianCalculator.CalculateMedian(StudentModel.studentsList, stud => stud.prelim);
+            double midterm = MedianCalculator.CalculateMedian(StudentModel.studentsList, stud => stud.midterm);
+            double finals = MedianCalculator.CalculateMedian(StudentModel.studentsList, stud => stud.finals);
+
+
+            label6.Text = $"Prelim Median: {prelim:F3}";
+            label6.Visible = true;
+
+
+            label5.Text = $"Midterm Median: {midterm:F3}";
+            label5.Visible = true;
+
+
+            label4.Text = $"Finals Median: {finals:F3}";
+            label4.Visible = true;
+        }
+
+        private void CalculateMode_BTN_Click(object sender, EventArgs e)
+        {
+            double prelim = ModeCalculator.HasMode(StudentModel.studentsList, stud => stud.prelim);
+            double midterm = ModeCalculator.HasMode(StudentModel.studentsList, stud => stud.midterm);
+            double finals = ModeCalculator.HasMode(StudentModel.studentsList, stud => stud.finals);
+
+
+            label9.Text = $"Prelim Mode: {prelim:F3}";
+            label9.Visible = true;
+
+
+            label8.Text = $"Midterm Mode: {midterm:F3}";
+            label8.Visible = true;
+
+
+            label7.Text = $"Finals Mode: {finals:F3}";
+            label7.Visible = true;
         }
     }
 }
