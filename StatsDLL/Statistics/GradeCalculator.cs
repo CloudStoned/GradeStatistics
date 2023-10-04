c7e66cf96918280b9ae7710e583b9d59b1c320de
CloudStone <cloudneo0719@gmail.com>
2023-10-04 16:35:49 +0800

    Removed Unecessary Things

--- StatsDLL/Statistics/GradeCalculator.cs
+++ StatsDLL/Statistics/GradeCalculator.cs
@@ -2,32 +2,10 @@
 {
     public class GradeCalculator
     {
-        private static double CalculateFinalPeriod(double prelim, double midterm, double finals)
+        public static double CalculateFinalPeriod(double prelim, double midterm, double finals)
         {
             double roundedUpFinalGrade = prelim + midterm + finals;
             return Math.Ceiling(roundedUpFinalGrade / 3);
         }
-
-        public static void DisplayGrade(List<StudentModel> students)
-        {
-            Console.WriteLine("-----------------------------------------------------------------------------------|");
-            Console.WriteLine("|                                    Students Grade                                |");
-            Console.WriteLine("------------------------------------------------------------------------------------");
-            Console.WriteLine("| Student Name |    Prelim    |    Midterm    |    Finals     |   Final Average    |");
-            Console.WriteLine("------------------------------------------------------------------------------------");
-
-            foreach (var student in students)
-            {
-                double roundDownPrelim = Math.Floor(student.prelim);
-                double roundDownMidterm = Math.Floor(student.midterm);
-                double roundDownFinals = Math.Floor(student.finals);
-                double roundedUpFinalGrade = CalculateFinalPeriod(roundDownPrelim, roundDownMidterm, roundDownFinals);
-
-                Console.WriteLine($"| {student.studentName,-12} | {student.prelim,13:F2} | {student.midterm,13:F2} | {student.finals,13:F2} | {roundedUpFinalGrade,17:F2} |");
-                Console.WriteLine("------------------------------------------------------------------------------------");
-            }
-
-        }
-
     }
 }
